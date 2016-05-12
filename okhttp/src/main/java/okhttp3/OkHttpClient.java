bd37786000128759a26db5471dfa728e4ea67209
Greg Lu <greg.lu@gmail.com>
2016-05-12 17:31:14 -0400

    Implemented user agent setting and DNS round robin support

--- okhttp/src/main/java/okhttp3/OkHttpClient.java
+++ okhttp/src/main/java/okhttp3/OkHttpClient.java
@@ -38,10 +38,26 @@ import okhttp3.internal.InternalCache;
 import okhttp3.internal.Platform;
 import okhttp3.internal.RouteDatabase;
 import okhttp3.internal.Util;
+import okhttp3.internal.Version;
 import okhttp3.internal.http.StreamAllocation;
 import okhttp3.internal.io.RealConnection;
 import okhttp3.internal.tls.CertificateChainCleaner;
 import okhttp3.internal.tls.OkHostnameVerifier;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.UnknownHostException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Factory for {@linkplain Call calls}, which can be used to send HTTP requests and read their
@@ -157,6 +173,9 @@ public class OkHttpClient implements Cloneable, Call.Factory {
   final int readTimeout;
   final int writeTimeout;
 
+  // @swoop
+  final String userAgent;
+
   public OkHttpClient() {
     this(new Builder());
   }
@@ -201,6 +220,9 @@ public class OkHttpClient implements Cloneable, Call.Factory {
     this.connectTimeout = builder.connectTimeout;
     this.readTimeout = builder.readTimeout;
     this.writeTimeout = builder.writeTimeout;
+
+    // @swoop
+    this.userAgent = builder.userAgent;
   }
 
   private X509TrustManager systemDefaultTrustManager() {
@@ -339,6 +361,13 @@ public class OkHttpClient implements Cloneable, Call.Factory {
   }
 
   /**
+   * @swoop
+   */
+  public String getUserAgent() {
+    return userAgent;
+  }
+
+  /**
    * Prepares the {@code request} to be executed at some point in the future.
    */
   @Override public Call newCall(Request request) {
@@ -376,6 +405,9 @@ public class OkHttpClient implements Cloneable, Call.Factory {
     int readTimeout;
     int writeTimeout;
 
+    // @swoop
+    String userAgent;
+
     public Builder() {
       dispatcher = new Dispatcher();
       protocols = DEFAULT_PROTOCOLS;
@@ -395,6 +427,9 @@ public class OkHttpClient implements Cloneable, Call.Factory {
       connectTimeout = 10_000;
       readTimeout = 10_000;
       writeTimeout = 10_000;
+
+      // @swoop
+      userAgent = Version.userAgent();
     }
 
     Builder(OkHttpClient okHttpClient) {
@@ -423,6 +458,9 @@ public class OkHttpClient implements Cloneable, Call.Factory {
       this.connectTimeout = okHttpClient.connectTimeout;
       this.readTimeout = okHttpClient.readTimeout;
       this.writeTimeout = okHttpClient.writeTimeout;
+
+      // @swoop
+      this.userAgent = okHttpClient.userAgent;
     }
 
     /**
@@ -781,6 +819,14 @@ public class OkHttpClient implements Cloneable, Call.Factory {
 
     public Builder addNetworkInterceptor(Interceptor interceptor) {
       networkInterceptors.add(interceptor);
+      return this;
+    }
+
+    /**
+     * @swoop
+     */
+    public Builder userAgent(String userAgent) {
+      this.userAgent = userAgent;
       return this;
     }
 
---
+++ okhttp/src/main/java/okhttp3/RoundRobinDns.java
@@ -0,0 +1,29 @@
+package okhttp3;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * For connections to origin servers that resolve to multiple IP addresses (sometimes known
+ * as DNS round robin load balancing), set this property to true in order to have a route
+ * randomly selected (as opposed to the order in which the DNS was resolved). This does not
+ * affect the way different routes are retried upon connection failures, only the order in
+ * which they're selected.
+ * <p/>
+ * This is especially useful given the way JVMs cache DNS resolutions (sometimes for the
+ * lifetime of the JVM session).
+ * <p/>
+ * This setting is defaulted to false.
+ */
+public class RoundRobinDns implements Dns {
+  @Override
+  public List<InetAddress> lookup(String hostname) throws UnknownHostException {
+    if (hostname == null) throw new UnknownHostException("hostname == null");
+    List<InetAddress> addresses = Arrays.asList(InetAddress.getAllByName(hostname));
+    Collections.shuffle(addresses);
+    return addresses;
+  }
+}
